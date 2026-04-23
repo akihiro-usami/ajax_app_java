1e14e575813d649f07157cdd2f95330aa9708176
akihiro-usami <akihiro.usami@smart-group.co.jp>
2026-04-23 09:09:13 +0900

    リクエストを処理する

--- src/main/java/in/tech_camp/ajax_app_java/repository/PostRepository.java
+++ src/main/java/in/tech_camp/ajax_app_java/repository/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.ajax_app_java.entity.PostEntity;
@@ -13,6 +14,10 @@ public interface PostRepository {
   @Select("SELECT * FROM posts ORDER BY created_at DESC")
   List<PostEntity> findAll();
 
+  @Select("SELECT * FROM posts WHERE id = #{id}")
+  PostEntity findById(Integer id);
+
   @Insert("INSERT INTO posts (content) VALUES (#{content})")
+  @Options(useGeneratedKeys=true, keyProperty="id")
   void insert(PostEntity post);
 }
