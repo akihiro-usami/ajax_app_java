8122a7679df62bf56901585faf3987885885aac5
akihiro-usami <akihiro.usami@smart-group.co.jp>
2026-04-23 09:24:05 +0900

    JSでレスポンスを処理する

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,3 +1,17 @@
+function buildHTML(xhr) {
+    const item = xhr.response;
+    const html = `
+        <div class="post">
+            <div class="post-date">
+                投稿日時：${item.createdAt}
+            </div>
+            <div class="post-content">
+                ${item.content}
+            </div>
+        </div>`;
+    return html;
+}
+
 function post() {
     const submit = document.getElementById("submit");
     submit.addEventListener("click", (e) => {
@@ -5,11 +19,20 @@ function post() {
 
         const form = document.getElementById("form");
         const formData = new FormData(form);
-
         const xhr = new XMLHttpRequest();
         xhr.open("POST", "/posts", true);
         xhr.responseType = "json";
         xhr.send(formData);
+        xhr.onload = () => {
+            if (xhr.status != 200) {
+                alert(`Error ${xhr.status}: ${xhr.response.error}`);
+                return;
+            }
+            const list = document.getElementById("list");
+            const formText = document.getElementById("content");
+            list.insertAdjacentHTML("afterend", buildHTML(xhr));
+            formText.value = "";
+        };
     });
 }
 
